52c79dfc19cce5344f48a8a02930e658275873c7
amineennaciri <spitlight.enn@gmail.com>
2023-12-26 17:57:52 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config();//this is what looks at the dotenv file, gets evers single environment variable in there, loads them up into your environmnet and now you can access them on process.env we do this in index.ts because this is the entry point to our server, so we want these envs to load up immediately so the rest of the app can use them.
+
 import app from './server';
 const PORT = 3001;
 app.listen(PORT, () => {
